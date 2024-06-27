8c811c69501ad3f00dd44b51446c9ec5cc16c6f8
Jakub <jakub.okninski@onet.pl>
2024-06-27 17:48:43 +0200

    wstęp do modeli

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Dydaktycznie.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Dydaktycznie.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+        public DbSet<QuestionAnswer> QuestionAnswers { get; set; }
+    }
+}
