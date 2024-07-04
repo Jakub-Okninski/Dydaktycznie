16803fefb359d58a2b9cbd49f4127c0a51e27b04
Jakub-Okninski <jakub.okninski@onet.pl>
2024-07-04 19:51:34 +0200

    logowanie test ;>

--- Controllers/PresentationsController.cs
+++ Controllers/PresentationsController.cs
@@ -18,6 +18,8 @@ namespace Dydaktycznie.Controllers
         public PresentationsController(ApplicationDbContext context)
         {
             _context = context;
+        
+        
         }
 
 
